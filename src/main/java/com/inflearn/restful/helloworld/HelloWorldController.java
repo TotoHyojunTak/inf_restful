4082e8f6df499b1de50d6f3c6ef8ca63252b6c24
hyojuntak <thj0309@hotmail.com>
2022-03-13 13:27:31 +0900

    User 도메인 클래스 생성

--- src/main/java/com/inflearn/restful/helloworld/HelloWorldController.java
+++ src/main/java/com/inflearn/restful/helloworld/HelloWorldController.java
@@ -1,4 +1,4 @@
-package com.inflearn.restful;
+package com.inflearn.restful.helloworld;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
