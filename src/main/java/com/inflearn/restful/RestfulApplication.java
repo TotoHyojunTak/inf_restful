280719e35170cf54793bf88be328b0e41d23b298
thj0309 <thj0309@cj.net>
2022-03-23 16:12:51 +0900

    gitignore & redis cachging

--- src/main/java/com/inflearn/restful/RestfulApplication.java
+++ src/main/java/com/inflearn/restful/RestfulApplication.java
@@ -2,12 +2,13 @@ package com.inflearn.restful;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.servlet.LocaleResolver;
 import org.springframework.web.servlet.i18n.SessionLocaleResolver;
 
 import java.util.Locale;
-
+@EnableCaching
 @SpringBootApplication
 public class RestfulApplication {
 
